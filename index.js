2489b6fdcd8e6cd182ad8b55b2f3209c3389fc90
NathanCarr87 <carr.excel@gmail.com>
2021-01-26 21:22:41 -0800

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
+import * as BABYLON from "babylonjs";
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+var createScene = function() {
+  var scene = new BABYLON.Scene(engine);
+
+  const camera = new BABYLON.ArcRotateCamera(
+    "camera",
+    -Math.PI / 2,
+    Math.PI / 2.5,
+    3,
+    new BABYLON.Vector3(0, 0, 0),
+    scene
+  );
+  camera.attachControl(canvas, true);
+  const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  );
+
+  return scene;
+};
+
+const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+// Add your code here matching the playground format
+const scene = createScene(); //Call the createScene function
+const ground = BABYLON.MeshBuilder.CreateGround("ground", {
+  width: 10,
+  height: 10
+});
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function() {
+  scene.render();
+});
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function() {
+  engine.resize();
+});
